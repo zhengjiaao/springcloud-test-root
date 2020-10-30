1f2d103cd45108904dcd124bd458f06ec855fb22
Zhengjiaao <zhengja@dist.com.cn>
2020-10-30 17:42:47 +0800

    fix: 调整

---
+++ cloud-nacos/nacos-config/src/main/java/com/zja/controller/HelloWorldController.java
@@ -0,0 +1,30 @@
+package com.zja.controller;
+
+import com.zja.entity.MyAttributes;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Company: 上海数慧系统技术有限公司
+ * Department: 数据中心
+ * Date: 2020-04-22 9:52
+ * Author: zhengja
+ * Email: zhengja@dist.com.cn
+ * Desc：
+ */
+@RefreshScope  //自动刷新nacos配置
+@RestController
+public class HelloWorldController {
+
+    @Autowired
+    private MyAttributes myAttributes;
+
+    @GetMapping("v1/hello")
+    public Object hello() {
+        return myAttributes;
+    }
+
+}
---
+++ cloud-nacos/nacos-provider/src/main/java/com/zja/NacosProviderApplication.java
@@ -0,0 +1,13 @@
+package com.zja;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@EnableDiscoveryClient  //注解表明是一个 Nacos 客户端
+@SpringBootApplication
+public class NacosProviderApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(NacosProviderApplication.class, args);
+    }
+}
