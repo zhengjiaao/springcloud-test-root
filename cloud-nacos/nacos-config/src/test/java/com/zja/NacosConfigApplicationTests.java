3b11ee10548dbdd5721d12cc83afcbcee7c4b3ba
Zhengjiaao <zhengja@dist.com.cn>
2021-10-20 16:12:41 +0800

    fix 修改配置

--- cloud-nacos/nacos-config/src/test/java/com/zja/NacosConfigApplicationTests.java
+++ cloud-nacos/nacos-config/src/test/java/com/zja/NacosConfigApplicationTests.java
@@ -1,13 +1,19 @@
 package com.zja;
 
+import com.zja.entity.MyAttributes;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class NacosConfigApplicationTests {
 
+    @Autowired
+    private MyAttributes myAttributes;
+
     @Test
     void contextLoads() {
+        System.out.println(myAttributes);
     }
 
 }
