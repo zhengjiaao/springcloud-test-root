b24dfe99f21fb998ff70bd0d236ca4ae8b844aad
Zhengjiaao <zhengja@dist.com.cn>
2021-10-21 14:37:47 +0800

    feat 新增-cloud-web模块

---
+++ cloud-web-8099/src/main/java/com/zja/CloudWebApplication.java
@@ -0,0 +1,13 @@
+package com.zja;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudWebApplication.class, args);
+	}
+
+}
