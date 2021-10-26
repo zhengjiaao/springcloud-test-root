49cbc83e8f5e7259dca5c50ec4d9ee8e4dbcb7cf
Zhengjiaao <zhengja@dist.com.cn>
2021-10-26 13:34:14 +0800

    fix 修改变动

---
+++ cloud-commons/README.md
@@ -0,0 +1,12 @@
+# cloud-commons 
+
+> 官方文档  
+> spring-cloud-commons 抽象 解析
+
+- [spring-cloud-commons/docs](https://docs.spring.io/spring-cloud-commons/docs/current/reference/html/)
+
+- DiscoveryClient 接口 服务发现
+- ServiceRegistry 接口 服务注册
+
+- ServiceInstanceChooser 接口 客户端负载均衡
+- EnableCircuitBreaker 接口 断路器功能
---
+++ cloud-commons/src/main/java/com/zja/CommonsApplication.java
@@ -0,0 +1,15 @@
+package com.zja;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class CommonsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommonsApplication.class, args);
+	}
+
+}
