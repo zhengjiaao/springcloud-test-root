b38080db3722a535894f69c834fbe885d8fe1e44
Zhengjiaao <zhengja@dist.com.cn>
2021-11-18 09:05:55 +0800

    fix(cloud-eureka-server) 优化注释

--- cloud-eureka/cloud-eureka-server/src/main/java/com/zja/EurekaServerApplication.java
+++ cloud-eureka/cloud-eureka-server/src/main/java/com/zja/EurekaServerApplication.java
@@ -4,6 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+/**
+ * eureka-server 注册中心服务端地址 ： http://localhost:16000/eureka/
+ */
 @SpringBootApplication
 @EnableEurekaServer //注解启动一个服务注册中心提供给其他应用进行对话
 public class EurekaServerApplication {
